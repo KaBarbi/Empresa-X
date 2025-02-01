61bb44d0e0e93fa0df08ffd01c04240d99409876
KaBarbi <kabarbizz@gmail.com>
2025-02-01 16:36:36 -0300

    [FEAT]: home content

---
+++ scripts/banner.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".slider img");
+  const navLinks = document.querySelectorAll(".slider-nav a");
+  const sliderContainer = document.querySelector(".slider");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          let activeSlideId = entry.target.id;
+
+          // Remove 'active' de todos os links
+          navLinks.forEach((link) => link.classList.remove("active"));
+
+          // Adiciona 'active' no link correspondente
+          const activeNav = document.querySelector(
+            `.slider-nav a[href="#${activeSlideId}"]`
+          );
+          if (activeNav) {
+            activeNav.classList.add("active");
+          }
+        }
+      });
+    },
+    { threshold: 0.75 }
+  );
+
+  navLinks.forEach((link, index) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const targetSlide = slides[index];
+      if (targetSlide) {
+        sliderContainer.scrollTo({
+          left: targetSlide.offsetLeft,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+
+  slides.forEach((slide) => observer.observe(slide));
+});
